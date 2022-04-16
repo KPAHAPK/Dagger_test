2380ba09e918077e73c6294fda0c12acc94e9599
KPAHAPK <Roman444.zaharov@gmail.com>
2022-04-16 19:29:21 +0300

    ElementsIntoSet dagger initialization for add Set into Set

--- app/src/main/java/com/example/dagger_1/di/EventModule.kt
+++ app/src/main/java/com/example/dagger_1/di/EventModule.kt
@@ -1,10 +1,12 @@
 package com.example.dagger_1.di
 
+import com.example.dagger_1.DatabaseHelper
 import com.example.dagger_1.events.Analytics
 import com.example.dagger_1.events.EventHandler
 import com.example.dagger_1.events.Logger
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.ElementsIntoSet
 import dagger.multibindings.IntoSet
 
 @Module
@@ -17,4 +19,9 @@ class EventModule {
     @IntoSet
     @Provides
     fun providesLogger(): EventHandler = Logger()
+
+    @ElementsIntoSet
+    @Provides
+    fun providesDatabaseHelperEventHandlers(databaseHelper: DatabaseHelper) : Set<EventHandler> =
+        databaseHelper.getEventHandlers()
 }
