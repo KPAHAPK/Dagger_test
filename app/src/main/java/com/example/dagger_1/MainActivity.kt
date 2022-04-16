a56b58c5bdf9828108b471480bdabc04fc699e19
KPAHAPK <Roman444.zaharov@gmail.com>
2022-04-16 18:46:22 +0300

    Lazy dagger initialization

--- app/src/main/java/com/example/dagger_1/MainActivity.kt
+++ app/src/main/java/com/example/dagger_1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.dagger_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.Lazy
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var databaseHelper: DatabaseHelper
     @Inject
     lateinit var networkUtils: NetworkUtils
+    //Only one object
+    private lateinit var networkUtilsLazy: Lazy<NetworkUtils>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,7 @@ class MainActivity : AppCompatActivity() {
         val appComponent = (application as App).appComponent
         appComponent.injectMainActivity(this)
         val mainActivityPresenter = appComponent.getMainActivityPresenter()
+        networkUtilsLazy = appComponent.getNetworkUtils()
+        val networkUtils = networkUtilsLazy.get()
     }
 }
--- app/src/main/java/com/example/dagger_1/di/AppComponent.kt
+++ app/src/main/java/com/example/dagger_1/di/AppComponent.kt
@@ -2,7 +2,9 @@ package com.example.dagger_1.di
 
 import com.example.dagger_1.MainActivity
 import com.example.dagger_1.MainActivityPresenter
+import com.example.dagger_1.NetworkUtils
 import dagger.Component
+import dagger.Lazy
 
 @Component(modules = [
     StorageModule::class,
@@ -11,4 +13,5 @@ import dagger.Component
 interface AppComponent {
     fun injectMainActivity(mainActivity: MainActivity)
     fun getMainActivityPresenter(): MainActivityPresenter
+    fun getNetworkUtils(): Lazy<NetworkUtils>
 }
